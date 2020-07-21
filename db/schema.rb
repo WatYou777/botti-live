9ccb04af9dc2a5c5ced6ce13bc900416a9de2907
wat <wat.tun.music777@gmail.com>
2020-07-21 13:25:39 +0000

    post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_18_060041) do
+ActiveRecord::Schema.define(version: 2020_07_19_093244) do
+
+  create_table "microposts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -20,4 +28,5 @@ ActiveRecord::Schema.define(version: 2020_07_18_060041) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "microposts", "users"
 end
