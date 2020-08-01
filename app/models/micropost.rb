dcd328e6e40138aac24a21bf7730500a61e74e17
wat <wat.tun.music777@gmail.com>
2020-08-01 08:21:41 +0000

    model create

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,7 @@ class Micropost < ApplicationRecord
   belongs_to :user
   
   validates :content, presence: true, length: { maximum: 255 }
+  
+  has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'micropost_id'
+  has_many :favoriters, through: :reverses_of_favorite, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,11 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  
+  has_many :favorites
+  has_many :favoritings, through: :favorites, source: :micropost
 
+  
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
